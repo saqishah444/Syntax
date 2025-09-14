042b8705564ce66317f2210d7de83fff5df9c7fc
saqishah444 <syedsaqibbukhari055@gmail.com>
2025-09-14 21:55:59 +0500

    home page complete

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import { Button } from "antd";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, Facebook, Twitter, Instagram, Dribbble, Linkedin } from "lucide-react";
 import homeheroimg from '../assets/homepage_hero.png'
 import vector from '../assets/Vector.png'
 import service1 from '../assets/services1.png'
@@ -13,7 +13,7 @@ import whatwedo3 from '../assets/whatwedo3.jpg'
 import whatwedo4 from '../assets/whatwedo4.jpg'
 
 
- 
+
 const Home = () => {
   return (
     <div>
@@ -162,98 +162,164 @@ const Home = () => {
 
           {/* Projects Grid */}
           <div className="grid justify-center gap-8 sm:grid-cols-2 lg:grid-cols-2">
-            {/* Project 1 */}
-            <div>
-              <div className="bg-white w-72 shadow-md hover:shadow-lg hover:translate-y-[-10px] duration-300 rounded-lg overflow-hidden">
-              <a href="" className="">
-                <img
-                src={whatwedo1}
-                alt="SnapChat Clone"
-                className="w-full h-48 object-cover"
-              />
-              <div className="p-4">
-                <h3 className="text-lg font-medium text-gray-900">SnapChat Clone</h3>
-                <a
-                  href="#"
-                  className="text-sm text-indigo-600 mt-2 inline-block hover:underline"
-                >
-                  View Project
-                </a>
+            <div className="flex flex-col gap-5">
+              {/* Project 1 */}
+              <div>
+                <div className="bg-white w-72 shadow-md hover:shadow-lg hover:translate-y-[-10px] duration-300 rounded-lg overflow-hidden">
+                  <a href="" className="">
+                    <img
+                      src={whatwedo1}
+                      alt="SnapChat Clone"
+                      className="w-full h-48 object-cover"
+                    />
+                    <div className="p-4">
+                      <h3 className="text-lg font-medium text-gray-900">SnapChat Clone</h3>
+                      <a
+                        href="#"
+                        className="text-sm text-indigo-600 mt-2 inline-block hover:underline"
+                      >
+                        View Project
+                      </a>
+                    </div>
+                  </a>
+                </div>
               </div>
-              </a>
-            </div>
+
+              {/* Project 2 */}
+              <div className="">
+                <div className="bg-white w-72 shadow-md hover:shadow-lg hover:translate-y-[-10px] duration-300 rounded-lg overflow-hidden">
+                  <a href="">
+                    <img
+                      src={whatwedo3}
+                      alt="Drumloop Production"
+                      className="w-full h-48 object-cover"
+                    />
+                    <div className="p-4">
+                      <h3 className="text-lg font-medium text-gray-900">
+                        Drumloop Production
+                      </h3>
+                      <a
+                        href="#"
+                        className="text-sm text-indigo-600 mt-2 inline-block hover:underline"
+                      >
+                        View Project
+                      </a>
+                    </div>
+                  </a>
+                </div>
+              </div>
             </div>
 
-            {/* Project 2 */}
-           <div className="mt-20">
-             <div className="bg-white w-72 shadow-md hover:shadow-lg hover:translate-y-[-10px] duration-300 rounded-lg overflow-hidden">
-              <a href="">
-                <img
-                src={whatwedo2}
-                alt="Drumloop Production"
-                className="w-full h-48 object-cover"
-              />
-              <div className="p-4">
-                <h3 className="text-lg font-medium text-gray-900">
-                  Drumloop Production
-                </h3>
-                <a
-                  href="#"
-                  className="text-sm text-indigo-600 mt-2 inline-block hover:underline"
-                >
-                  View Project
-                </a>
+            <div className="flex flex-col gap-5 mt-0 md:mt-20">
+              {/* Project 3 */}
+              <div>
+                <div className="bg-white w-72 shadow-md hover:shadow-lg hover:translate-y-[-10px] duration-300 rounded-lg overflow-hidden">
+                  <a href="">
+                    <img
+                      src={whatwedo2}
+                      alt="Pagoda Partners"
+                      className="w-full h-48 object-cover"
+                    />
+                    <div className="p-4">
+                      <h3 className="text-lg font-medium text-gray-900">Pagoda Partners</h3>
+                      <a
+                        href="#"
+                        className="text-sm text-indigo-600 mt-2 inline-block hover:underline"
+                      >
+                        View Project
+                      </a>
+                    </div>
+                  </a>
+                </div>
               </div>
-              </a>
-            </div>
-           </div>
 
-            {/* Project 3 */}
-            <div>
-              <div className="bg-white w-72 shadow-md hover:shadow-lg hover:translate-y-[-10px] duration-300 rounded-lg overflow-hidden">
-              <a href="">
-                 <img
-                src={whatwedo3}
-                alt="Pagoda Partners"
-                className="w-full h-48 object-cover"
-              />
-              <div className="p-4">
-                <h3 className="text-lg font-medium text-gray-900">Pagoda Partners</h3>
-                <a
-                  href="#"
-                  className="text-sm text-indigo-600 mt-2 inline-block hover:underline"
-                >
-                  View Project
-                </a>
+              {/* Project 4 */}
+              <div>
+                <div className="bg-white w-72 shadow-md hover:shadow-lg hover:translate-y-[-10px] duration-300 rounded-lg overflow-hidden">
+                  <a href="">
+                    <img
+                      src={whatwedo4}
+                      alt="E-learning App"
+                      className="w-full h-48 object-cover"
+                    />
+                    <div className="p-4">
+                      <h3 className="text-lg font-medium text-gray-900">E-learning App</h3>
+                      <a
+                        href="#"
+                        className="text-sm text-indigo-600 mt-2 inline-block hover:underline"
+                      >
+                        View Project
+                      </a>
+                    </div>
+                  </a>
+                </div>
               </div>
-              </a>
             </div>
-            </div>
-
-            {/* Project 4 */}
-            <div>
-              <div className="bg-white w-72 shadow-md hover:shadow-lg hover:translate-y-[-10px] duration-300 rounded-lg overflow-hidden">
-             <a href="">
-              <img
-                src={whatwedo4}
-                alt="E-learning App"
-                className="w-full h-48 object-cover"
-              />
-              <div className="p-4">
-                <h3 className="text-lg font-medium text-gray-900">E-learning App</h3>
-                <a
-                  href="#"
-                  className="text-sm text-indigo-600 mt-2 inline-block hover:underline"
-                >
-                  View Project
-                </a>
-              </div>
-             </a>
-            </div>
-            </div>
+          </div>
+          <div className="flex justify-center">
+            <button
+              className=" py-3 px-16 mt-5 md:mt-10 text-white rounded-full bg-pink-400 hover:bg-pink-500 border-none font-semibold transition-all duration-300"
+            >
+              More
+            </button>
           </div>
         </div>
 
+      </section>
+      <section id="connect">
+        <div>
+          <div className="flex flex-col items-center justify-center py-0 md:py-40 bg-white">
+            <h1 className="text-3xl md:text-5xl font-semibold text-gray-800 text-center">
+              We can’t wait to work <br /> with <span className="text-purple-600">You</span>
+            </h1>
+            <button className="mt-6 px-8 py-3 text-[18px] font-semibold  bg-pink-400 text-white rounded-full shadow hover:bg-pink-500 transition duration-300">
+              Lets connect
+            </button>
+          </div>
+        </div>
+
+      </section>
+      <section id="footer" className="bg-gray-100 py-10 px-4 sm:px-8 md:px-16 mt-3">
+        <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-10 md:gap-8">
+          {/* Syntrax Info */}
+          <div className="flex flex-col items-start md:items-start text-center md:text-left">
+            <h3 className="font-semibold  text-gray-900 text-2xl mb-2">Syntrax</h3>
+            <p className="text-gray-800 text-[19px] mb-4 max-w-xs md:max-w-none">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Leo pulvinar sit sed eu nulla risus quis amet.</p>
+            <div className="flex justify-center md:justify-start gap-4 mt-2 text-xl text-gray-900 w-full">
+              <a href="#" aria-label="Facebook" className="hover:text-pink-400"><Facebook size={22} /></a>
+              <a href="#" aria-label="Twitter" className="hover:text-pink-400"><Twitter size={22} /></a>
+              <a href="#" aria-label="Instagram" className="hover:text-pink-400"><Instagram size={22} /></a>
+              <a href="#" aria-label="Dribbble" className="hover:text-pink-400"><Dribbble size={22} /></a>
+              <a href="#" aria-label="LinkedIn" className="hover:text-pink-400"><Linkedin size={22} /></a>
+            </div>
+          </div>
+          {/* Contact Info */}
+          <div className="flex flex-col items-start md:items-start text-center md:text-left">
+            <h3 className="font-semibold text-2xl text-gray-900 mb-2">Contact</h3>
+            <div className="flex items-center gap-2 text-gray-800 text-[19px] mb-2 justify-center md:justify-start w-full">
+              <span><i className="far fa-envelope"></i></span>
+              <span>contactatsytrax@gmail.com</span>
+            </div>
+            <div className="flex items-center gap-2 text-gray-800 text-[20px] justify-center md:justify-start w-full">
+              <span><i className="fas fa-phone"></i></span>
+              <span>+91 6266625558</span>
+            </div>
+          </div>
+          {/* About Links */}
+          <div className="flex flex-col items-start md:items-start text-center md:text-left">
+            <h3 className="font-semibold text-2xl text-gray-900 mb-2">About</h3>
+            <ul className="text-gray-800 text-[19px]  space-y-2">
+              <li><a href="#" className="hover:underline hover:text-pink-400">About Us</a></li>
+              <li><a href="#" className="hover:underline hover:text-pink-400">Contact us</a></li>
+            </ul>
+          </div>
+        </div>
+        <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between mt-8 pt-6  text-gray-600 text-[14px]">
+          <div className="flex items-center gap-2 mb-2 md:mb-0 justify-center md:justify-start w-full">
+            <span><i className="far fa-copyright"></i></span>
+            <span>2020 Syntrax.in All Rights Reserved.</span>
+          </div>
+        </div>
       </section>
 
 
